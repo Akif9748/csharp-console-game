59ab7d09f864e54fb03a2b4629d1a01a1df226e9
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-02-16 23:41:56 +0300

    Add files via upload

---
+++ ConsoleGameEngine/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+
+namespace ConsoleGameTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Config of game
+            int[] pos = { 0, 0 };
+            char character = ' ';
+
+            //Count of key presses:
+            int moves = 0;
+
+            ConsoleGame game = new ConsoleGame();
+            ConsoleKeyInfo keyInfo;
+
+            while (true)
+            {
+
+                //First render:
+                game.Set(pos[0], pos[1], character);
+
+                keyInfo = Console.ReadKey();
+
+                //For exit:
+                if (keyInfo.KeyChar == 'c')
+                {
+                    game.Clear();
+                    Console.WriteLine("Game finished with " + moves + " moves.");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+
+                    //Reset map:
+                    game.Reset();
+
+                    //Change pos:
+                    switch (keyInfo.Key.ToString())
+                    {
+                        case "LeftArrow":
+                            if (pos[0] > 0) pos[0] -= 1;
+                            break;
+
+                        case "RightArrow":
+                            if (pos[0] < game.x - 1) pos[0] += 1;
+                            break;
+
+                        case "UpArrow":
+                            if (pos[1] > 0) pos[1] -= 1;
+                            break;
+
+                        case "DownArrow":
+                            if (pos[1] < game.y - 1) pos[1] += 1;
+                            break;
+
+                        default:
+                            break;
+
+                    }
+
+                    //SET and RENDER:
+                    game.Set(pos[0], pos[1], character);
+                    moves++;
+                }
+
+            }
+        }
+    }
+}
